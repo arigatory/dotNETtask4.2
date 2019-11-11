bfb0f91b1bdbbae9a469d95d29b7ec9b19732656
arigatory <arigatory@gmail.com>
2019-11-11 12:28:43 +0300

    Add project files.

---
+++ dotNETtask4.2/Hexagon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNETtask4._2
+{
+    class Hexagon : Shape, IPoint
+    {
+        public int Point
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ dotNETtask4.2/Triangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNETtask4._2
+{
+    class Triangle : Shape, IPoint
+    {
+        public int Point => throw new NotImplementedException();
+    }
+}
